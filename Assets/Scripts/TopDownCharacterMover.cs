67eb4376e470f1e3c6970fbdab1f53cff00f10e6
Niveditha133 <69324426+Niveditha133@users.noreply.github.com>
2022-04-27 14:28:25 -0700

    Progress 2 - 0427

--- Assets/Scripts/TopDownCharacterMover.cs
+++ Assets/Scripts/TopDownCharacterMover.cs
@@ -7,10 +7,20 @@ public class TopDownCharacterMover : MonoBehaviour
 {
     private InputHandler _input;
     public GameObject prefab;
+
+    public LayerMask Mask;
+    public GameObject BlockGUIPrefab;
+    public GameObject BlockPrefab;
+
+    //set at the beginning 
+    private Camera _camera;
     private BlockSystem _blockSystem;
     private GameObject _blockGUI;
-    public GameObject BlockGUIPrefab;
+
+    //set at each frame
+    private bool _canBuild = false;
     private Vector3 _buildPos;
+    private int typeSelect = 0;
 
     [SerializeField]
     private float moveSpeed;
@@ -25,6 +35,7 @@ public class TopDownCharacterMover : MonoBehaviour
     private void Awake()
     {
         _input = GetComponent<InputHandler>();
+
     }
 
     // Update is called once per frame
@@ -37,13 +48,14 @@ public class TopDownCharacterMover : MonoBehaviour
             RotateTowardMovementVector(movementVector);
         else
             RotateTowardMouseVector();
+        StartBlock();
     }
- 
+
     private void RotateTowardMouseVector()
     {
         Ray ray = camera.ScreenPointToRay(_input.MousePosition);
 
-        if(Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
+        if (Physics.Raycast(ray, out RaycastHit hitInfo, maxDistance: 300f))
         {
             var target = hitInfo.point;
             target.y = transform.position.y;
@@ -53,7 +65,7 @@ public class TopDownCharacterMover : MonoBehaviour
 
     private void RotateTowardMovementVector(Vector3 movementVector)
     {
-        if(movementVector.magnitude == 0) { return; }
+        if (movementVector.magnitude == 0) { return; }
 
         var rotation = Quaternion.LookRotation(movementVector);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rotation, rotateSpeed * Time.deltaTime);
@@ -75,5 +87,64 @@ public class TopDownCharacterMover : MonoBehaviour
         var targetPosition = transform.position + targetVector * speed;
         transform.position = targetPosition;
         return targetVector;
+
+
+    }   
+
+        private void StartBlock()
+        {
+            _blockSystem = GetComponent<BlockSystem>();
+            _blockGUI = Instantiate(BlockGUIPrefab, _buildPos, Quaternion.identity);
+            //BuildBlock();
+        }
+
+        private void BuildBlock()
+        {
+            {
+                //find the blocks build location
+                RaycastHit hit; //shoot ray in direction of camera from centre of screen -- Why are we hitting from the centre of the screen, can that not be from the camera itself because that's where our blockmanager is??
+                if (Physics.Raycast(_camera.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2, 0)), out hit, 10, Mask)) // what is out hit??
+                {
+                    Vector3 pos = hit.point; //get hit position
+                    _buildPos = new Vector3(Mathf.Round(pos.x), Mathf.Round(pos.y), Mathf.Round(pos.z)); //keep the block in a grid
+                    _canBuild = true;
+                    _blockGUI.transform.position = _buildPos;//update transparent cube position
+                }
+                else
+                {
+                    _canBuild = false;
+                }
+                
+        //loop through block types 
+        if (Input.GetMouseButton(1))
+
+        {
+            typeSelect++; //increment type select by 1, the same as typeselect = typeselect +1;
+            if (typeSelect >= _blockSystem.Blocks.Count)
+            {
+                typeSelect = 0;
+            }
+
+        }
+
+        //build blocks
+        if (_canBuild)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                PlaceBlock();
+            }
+        }
+    }
+
+    void PlaceBlock()
+    {
+        GameObject block = Instantiate(BlockPrefab, _buildPos, Quaternion.identity);
+        Block type = _blockSystem.Blocks[typeSelect];
+        block.name = type.BlockName;
+        block.GetComponent<MeshRenderer>().material = type.BlockMaterial; // can set speed, navmesh agent, target location - add thigs in struct and can replace the information which is on struct
+
     }
 }
+}
+
