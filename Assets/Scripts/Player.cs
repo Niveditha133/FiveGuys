f4403a1fdaf5191d385f71f30d9648b0590ee21c
code-sanya <sanyav@g.ucla.edu>
2022-05-02 13:05:01 -0700

    Revert "Character_download_5_2"
    This reverts commit 8dca170d0dff1307713cd64f6206762754087f42.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,25 +1,50 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
-    public Rigidbody rb;
-    public Transform cameraTransform;
-    public GameManager gameManager;
+    public float A = 0;
+    public float B = 1;
 
-    void Update()
+    [Range(0,1)] //way to modify - more useful
+    public float T = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
-        Vector3 forward = cameraTransform.forward;
-        Vector3 right = cameraTransform.right;
-        forward.y = 0;
-        right.y = 0;
-        forward.Normalize();
-        right.Normalize();
-        Vector3 direction = forward * moveVertical + right * moveHorizontal;
-        rb.AddForce(direction * 5);
+        
     }
 
+    //Vector3 move = new Vector3(0, 0, 0); //set only once at the beginning 
+
+    // Update is called once per frame
+    void Update()
+    {
+        //float lerp = Mathf.Lerp(A, B, T);
+
+        
+        float angle = 0;
+        float speed = 0;
+
+
+        if (Input.GetKey(KeyCode.LeftArrow)) { speed = -0.1f; }
+        if (Input.GetKey(KeyCode.RightArrow)) { speed = 1.1f; }
+        if (Input.GetKey(KeyCode.LeftArrow)) { angle -= 1f; }
+        if (Input.GetKey(KeyCode.RightArrow)) { angle += 1f; }
+        //if (Input.GetKey(KeyCode.UpArrow)) { move.y += 1.1f; }
+        //if (Input.GetKey(KeyCode.DownArrow)) { move.y -= 0.1f; }
+        //if (Input.GetKey(KeyCode.UpArrow)) { forward.y += 1f; }
+        //if (Input.GetKey(KeyCode.DownArrow)) { forward.y -= 1f; }
+
+        Vector3 move = this.transform.forward * speed;
+        
+
+        //this.transform.forward = forward;
+        this.transform.Rotate(Vector3.up, angle);
+        this.transform.Translate(move); //this.transform.position += move; //this.transform.position = move;
+        
+        //Debug.Log(T);
+    }
 }
